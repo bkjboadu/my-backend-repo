db9721f105ef3444b6b0c939f020375efe21b58a
bright <brbojr@gmail.com>
2024-09-27 02:09:53 -0600

    order management endpoints created

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("accounts/", include("user_management.urls")),
     path("inventory/", include("inventory_management.urls")),
     path("carts/", include("cart_management.urls")),
+    path('orders/',include("order_management.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
