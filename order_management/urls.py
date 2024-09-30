e1c5072e31a24e0127795e3550187268f9eed3d4
bright <brbojr@gmail.com>
2024-09-30 08:28:39 -0600

    order management order tracking updated

--- order_management/urls.py
+++ order_management/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
 
     path('list/', UserOrderListView.as_view(), name='order-list'),
 
-    path('<int:order_id>/status/', UpdateOrderStatusView.as_view(), name='update-order-status'),  # Endpoint: /orders/<order_id>/status/
+    path('<int:order_id>/status/', UpdateOrderStatusView.as_view(), name='update-order-status'),
 
-    path('<int:order_id>/payment/', PaymentView.as_view(), name='order-payment'),  # Endpoint: /orders/<order_id>/payment/
+    path('<int:order_id>/payment/', PaymentView.as_view(), name='order-payment'),
 
-    path('<int:order_id>/shipment/', ShipmentView.as_view(), name='order-shipment'),  # Endpoint: /orders/<order_id>/shipment/
+    path('<int:order_id>/shipment/', ShipmentView.as_view(), name='order-shipment'),
 
-    path('<int:order_id>/cancel/', CancelOrderView.as_view(), name='cancel-order'),  # Endpoint: /orders/<order_id>/cancel/
+    path('<int:order_id>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
 ]
