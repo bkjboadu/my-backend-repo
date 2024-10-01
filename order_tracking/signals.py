b4baab000b24958dad819edef358bc14e56ac282
amalitechpc396 <jmillicent135@gmail.com>
2024-10-01 15:53:26 +0000

    order tracking signals

---
+++ order_tracking/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import OrderTracking
+
+def update_order_status(sender, instance, **kwargs):
+    order = instance.order
+    if instance.status == 'delivered':
+        order.status = 'delivered'
+    elif instance.status == 'failed':
+        order.status = 'failed_delivery'
+    order.save()
