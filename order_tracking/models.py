caced52a5cc0db69fffcf1e6db51177607ad763e
bright <brbojr@gmail.com>
2024-10-02 14:08:42 -0600

    order tracking updated

--- order_tracking/models.py
+++ order_tracking/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from order_management.models import Order
 
-# Create your models here.
+class OrderTracking(models.Model):
+    STATUS_CHOICES = [
+        ("in_transit", "In Transit"),
+        ("delivered", "Delivered"),
+        ("failed", "Failed"),
+        ("pending", "Pending"),
+        ("out_for_delivery", "Out for Delivery"),
+    ]
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, related_name="tracking_info"
+    )
+    tracking_number = models.CharField(max_length=100, unique=True)
+    carrier = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    current_location = models.CharField(max_length=255, blank=True, null=True)
+    failure_reason = models.CharField(max_length=255, blank=True, null=True)
+    estimated_delivery = models.DateField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Tracking {self.tracking_number} for Order {self.order.order_number}"
--- order_tracking/urls.py
+++ order_tracking/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import TrackOrderView
+from .views import TrackOrderView, OrderTrackingDetailView, UpdateOrderTrackingStatus
 
 urlpatterns = [
-    path('orders/<int:order_id>/', TrackOrderView.as_view(), name='track-order'),
+    path("track_orders/<int:order_id>/", TrackOrderView.as_view(), name="track-order"),
+    path(
+        "admin_track_orders/<str:tracking_number>/",
+        OrderTrackingDetailView.as_view(),
+        name="admin-track-orders",
+    ),
+    path(
+        "update_tracked_orders/<str:tracking_number>/",
+        UpdateOrderTrackingStatus.as_view(),
+        name="update-tracked-orders",
+    ),
 ]
