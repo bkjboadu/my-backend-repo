67a043369e8609f7b9d5e13cadf90194942922cf
bright <brbojr@gmail.com>
2024-10-05 15:38:01 -0600

    STATIC_ROOT updated

--- backend/settings.py
+++ backend/settings.py
@@ -229,9 +229,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 
 # Static files (CSS, JavaScript, Images)
