15fbff1eb99950f18e00bfaf45e7729dae8febf6
bright <brbojr@gmail.com>
2024-10-11 15:09:36 -0600

    Configure Celery for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn backend.asgi:application -w 4 -k uvicorn.workers.UvicornWorker
+worker: celery -A backend worker --loglevel=info
--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
 
 # stripe details
 
