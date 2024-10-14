a26a9b64782b46442f32f086eda8fef684748cca
bright <brbojr@gmail.com>
2024-10-14 10:03:42 -0600

    celery updated

--- backend/celery.py
+++ backend/celery.py
@@ -5,3 +5,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",'backend.settings')
 app = Celery('backend')
 app.config_from_object('django.conf:settings',namespace='CELERY')
 app.autodiscover_tasks()
+
+
+#Restart workers after processing 100 tasks
+app.conf.worker_max_tasks_per_child = 100
+app.conf.worker_concurrency = 2
+app.conf.task_result_expires = 3600
+app.conf.task_soft_time_limit = 300
+app.conf.task_time_limit = 600
+app.conf.task_acks_late = True
