0b0be95e5d91d4ef3e4ae9001a39c774c70e78bc
bright <brbojr@gmail.com>
2024-12-01 22:41:47 -0700

    unused apps removed

--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("user_management.urls")),
     path("inventory/", include("inventory_management.urls")),
-    path("carts/", include("cart_management.urls")),
+    # path("carts/", include("cart_management.urls")),
     path("payments/", include("payment.urls")),
     path("orders/", include("order_management.urls")),
     path("track_orders/", include("order_tracking.urls")),
